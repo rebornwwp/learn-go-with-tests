8b1ffa2d0cbdea99cc4bd97ba325d7901b1cdf03
reborn <rebornwwp@gmail.com>
2020-09-11 01:10:06 +0800

    select

---
+++ selects/selects.go
@@ -0,0 +1,35 @@
+package selects
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var TenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, TenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+
+}
+func ping(url string) chan bool {
+	ch := make(chan bool)
+
+	go func() {
+		_, _ = http.Get(url)
+		ch <- true
+	}()
+
+	return ch
+}
